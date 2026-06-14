137ffe86582aaba7fd6e953cdb0d08c1c073ee44
MyTeleProject2026 <mmteam929@gmail.com>
2026-06-14 06:38:49 -0400

    update vite.config.js

--- frontend-admin/vite.config.js
+++ frontend-admin/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3001, proxy: { '/api': 'http://localhost:5001' } }
+  server: { port: 3001, proxy: { '/api': 'http://localhost:5001' } },
+  // Only add base if your site is not at the domain root
+  // base: '/',
 })
